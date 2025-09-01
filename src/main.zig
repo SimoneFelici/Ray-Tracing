cd03bb18622f1ccbeb6a8e2dc3581478a9fb4602
SimoneFelici <felicisimone9@gmail.com>
2025-09-01 16:23:07 +0200

    fixes

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,17 @@ const std = @import("std");
 pub fn main() !void {
     const img_width: u32 = 256;
     const img_height: u32 = 256;
-    var stdout_buffer: [1024]u8 = undefined;
-    var file_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &file_writer.interface;
+
+    var stdout_buffer: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
     try stdout.print("P3\n{d} {d}\n255\n", .{ img_width, img_height });
-    for (0..img_height) |j| {
-        for (0..img_width) |i| {
+
+    var j: u32 = 0;
+    while (j < img_height) : (j += 1) {
+        var i: u32 = 0;
+        while (i < img_width) : (i += 1) {
             const r = @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(img_width - 1));
             const g = @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(img_height - 1));
             const b = 0.0;
@@ -17,6 +21,7 @@ pub fn main() !void {
             const ir: u8 = @intFromFloat(255.999 * r);
             const ig: u8 = @intFromFloat(255.999 * g);
             const ib: u8 = @intFromFloat(255.999 * b);
+
             try stdout.print("{d} {d} {d}\n", .{ ir, ig, ib });
         }
     }
